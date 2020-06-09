03d0ded9d5419199eb66c61c2fe5c1c97f6901a5
ByIlsa <byilsa@aukjevanderwal.nl>
2020-06-09 07:30:25 +0000

    Added js script for navbar close on click on mobile

---
+++ scripts.js
@@ -0,0 +1,5 @@
+
+// js provided by Eventyret_mentor from Slack
+    $('.navbar-nav>a, .navbar-brand, .socials-burger').on('click', function(){
+        $('.navbar-collapse').collapse('hide');
+    });
